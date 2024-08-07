d68befeddd8027b6472048c5e40ec0e9d8e3e658
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-08-07 20:32:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,26 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
+  let project=[
+    {name:"retaurant Ui", description:"restaurant web developed"},
+    {name:"insta clone", description:"develop by html"},
+    {name:"youtube", description:"deveploped in 1999"}
+  ]
   return (
-    <div id="main"></div>
+    <div id="main" className="ns-wrapper">
+      {
+        project.map((elem,index)=>(
+         <div  key={index}>
+          {console.log(elem)}
+
+<h1 data-ns-test="project-name">{elem.name}</h1>
+<h6 data-ns-test="project-description">{elem.description}</h6>
+
+         </div>
+        ))
+      }
+    </div>
+
   )
 }
 
